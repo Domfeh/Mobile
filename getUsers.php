fad6ad4fc5ae279019643880eed7c7ac28befc55
Domfeh <41950595+Domfeh@users.noreply.github.com>
2022-03-21 20:06:31 +0000

    Update getUsers.php

--- getUsers.php
+++ getUsers.php
@@ -1,39 +1,18 @@
 <?php
- 
-// Importing DatabaseConfig.php file.
-include 'connection.php';
- 
-// Creating MySQL Connection.
- 
- // Getting the received ID in JSON Format into $json variable.
 
- 
- // Populate ID fraom JSON $obj array and store into $ID variable.
- //$email = $POST["Fs_email"];
- $email ="adwoa@gmail.com";
- //Fetching the selected record as per ID.
- 
-  $sql="select * from new_users  WHERE Fk_email='$email'";
-  $result=mysqli_query($conn,$sql);
+    include 'connection.php';
+    //$email='adwoa@gmail.com';
+    $email = $_POST["Fs_email"];
+    $sql="SELECT * FROM new_users  WHERE `Fk_email`='$email'";
+    $result=mysqli_query($conn, $sql);
 
-if ($result->num_rows >0) {
- 
- while($row= $result->fetch_assoc()) {
- $Ite[] = array($row);
- $res = json_encode($Ite);
- 
-
- }
- 
- echo $res;
-
-
-}else {
-	
- echo "No Results Found.";
- 
+    if($result ->num_rows > 0){
+    while($row=$result->fetch_assoc()){
+      $Ite[] = $row;
+      $res = json_encode($Ite);
+    }
+    echo $res;
+}else{
+  echo  "No Result Found";
 }
- 
- 
-
-?>
+?>   
